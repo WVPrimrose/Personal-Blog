d162239b00d951e1a1211e6743b76aaaf14beb15
Katie Buchholz <kzbindy@hotmail.com>
2024-04-13 09:48:50 -0400

    commented out the href to the next index page; I'm just going to take a break from this for now.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,11 @@
+const button = document.getElementById('submit')
+const nameInput = document.getElementById('username')
+const title = document.getElementById('blog-title')
+const content = document.getElementById('comment')
 let blogParts = []
 
 //creates blog post elements
-for (let i = 0; i < blogObject.length; i++) {
+for (let i = 0; i < blogParts.length; i++) {
 const username = document.createElement(h3);
 const blogTitle = document.createElement(h3);
 const blogContent = document.createElement(p);
@@ -15,25 +19,29 @@ const blogObject = {
     blogTitle: blogTitle,
     blogContent: blogContent,
 }
-blogParts.push(blogObject)
-localStorage.setItem('blogObject', JSON.stringify(blogObject));
 }
 
 //handles submit button on index.html
 function submit(event) {
     event.preventDefault();
+    const name = nameInput.value
+    const bT = title.value
+    const message = content.value
+    console.log(name)
+    console.log(bT)
+    console.log(message)
 }
-
+button.addEventListener('click', submit)
 //light-dark mode
 let mode = 'light';
 
-themeSwitcher.addEventListener('click', function (){
-    if (mode === 'light') {
-        mode = 'dark';
-        container.setAttribute('class', 'dark');
-    }
-    else {
-        mode = 'light';
-        constainer.setAttribute('class', 'light');
-    }
-})
+//themeSwitcher.addEventListener('click', function (){
+  //  if (mode === 'light') {
+    //    mode = 'dark';
+      //  container.setAttribute('class', 'dark');
+   // }
+   // else {
+     //   mode = 'light';
+       // constainer.setAttribute('class', 'light');
+//     }
+// })
